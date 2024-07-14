d9b34826cdd91186ac7ba93f99dd339952eabee5
mustafanafia <55895079+mustafanafia@users.noreply.github.com>
2024-07-14 19:48:37 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,59 @@
+/*
+Detect a cycle in a linked list. Note that the head pointer may be 'null' if the list is empty.
+
+A Node is defined as: 
+    class Node {
+        int data;
+        Node next;
+    }
+*/
+
+
+/*
+
+boolean hasCycle(Node head) {
+        HashSet<Node> hs = new HashSet<>();
+
+        while(head != null){
+        if(hs.contains(head)){
+        return true;
+        }
+        else{
+        hs.add(head);
+        head = head.next;
+        }
+        }
+        return false;
+        }
+
+
+        */
+
+
+                                //*** Floyd Warshall Algorithm solution ***
+
+/*
+
+
+
+boolean hasCycle(Node head) {
+
+        Node Rabbit = head;
+        Node Turtle = head;
+
+
+        while(Rabbit != null && Turtle != null && Rabbit.next != null){
+
+        Rabbit = Rabbit.next.next;
+        Turtle = Turtle.next;
+
+        if(Rabbit == Turtle){
+        return true;
+        }
+        }
+
+        return false;
+
+        }
+
+  */
